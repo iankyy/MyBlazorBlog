9b840f809c661104b2504a67b96e7e03b02aafb3
pedro <pedroianky2@gmail.com>
2024-05-31 14:51:40 -0300

    Add project files.

---
+++ MyBlazorBlog/Data/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyBlazorBlog.Data
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, MaxLength(100)]
+        public string Name { get; set; }
+
+
+
+    }
+}
